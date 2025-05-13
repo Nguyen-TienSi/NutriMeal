b2f616d4d8a3833c840e6ba3bb323620d616f6b7
h-i-e-u <hieudoanw11@gmail.com>
2025-05-13 11:07:57 +0700

    chore: change favicon, fix: image in recipe model

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:3000/api';
+// const API_BASE_URL = 'http://localhost:3000/api';
 
 export const apiRequest = async (endpoint, method = "GET", data = null, isFormData = false) => {
     const url = `${import.meta.env.VITE_API_BASE_URL}${endpoint}`;
